330f8524f5503c735ed311765a7a640166dd6834
Lasse <lasse.rosenthal@gmx.de>
2020-12-01 16:37:37 +0100

    constexpr BitProxy

--- UnitTests/src/include/BitFieldTest.h
+++ UnitTests/src/include/BitFieldTest.h
@@ -19,9 +19,10 @@
 // includes
 #include <Utils/BitField.h>
 
+#include <string>
 #include <sstream>
  
-
+using namespace std::string_literals;
 using namespace cpptools;
  
 TEST(BitField, types)
@@ -53,11 +54,72 @@ TEST(BitField, streamIntoOstream)
   using bitField = BitField<3ULL, 2ULL>;
   constexpr bitField b{0b0000'0110};
 
-  const std::string expected{"00011000"};
   std::stringstream sstr;
   sstr << b;
+  EXPECT_EQ(sstr.str(), "00011000"s);
+}
 
-  EXPECT_EQ(sstr.str(), expected);
+TEST(BitField, BitProxySetBitToTrue)
+{
+  using bitField = BitField<3ULL, 2ULL>;
+  bitField b{0b0000'0011};
+  std::stringstream sstr;
+  sstr << b;
+  EXPECT_EQ(sstr.str(), "00001100"s);
+
+  auto bitProxy = b.getBitProxy(2ULL);
+  bitProxy      = true;
+  sstr.str("");
+  sstr << b;
+  EXPECT_EQ(sstr.str(), "00011100"s);
+}
+
+TEST(BitField, BitProxySetBitToFalse)
+{
+  using bitField = BitField<3ULL, 2ULL>;
+  bitField b{0b0000'0111};
+  auto bitProxy = b.getBitProxy(0ULL);
+  bitProxy      = false;
+  std::stringstream sstr;
+  sstr << b;
+  EXPECT_EQ(sstr.str(), "00011000"s);
+}
+
+TEST(BitField, BitProxyFlipOneBit)
+{
+  using bitField = BitField<3ULL, 2ULL>;
+  bitField b{0b0000'0111};
+  auto bitProxy = b.getBitProxy(0ULL);
+  bitProxy.flip();
+  std::stringstream sstr;
+  sstr << b;
+  EXPECT_EQ(sstr.str(), "00011000"s);
+}
+
+TEST(BitField, BitProxyFlipZeroBit)
+{
+  using bitField = BitField<3ULL, 2ULL>;
+  bitField b{0b0000'0101};
+  auto bitProxy = b.getBitProxy(1ULL);
+  bitProxy.flip();
+  std::stringstream sstr;
+  sstr << b;
+  EXPECT_EQ(sstr.str(), "00011100"s);
+}
+
+TEST(BitField, BitProxyIsSet)
+{
+  using bitField = BitField<3ULL, 2ULL>;
+  constexpr bitField b{0b0000'0101};
+
+  constexpr auto bitProxy0 = b.getBitProxy(0ULL);
+
+  bool isZeroBitSet = bitProxy0;
+  EXPECT_TRUE(isZeroBitSet);
+  constexpr auto bitProxy1 = b.getBitProxy(1ULL);
+  EXPECT_FALSE(bitProxy1.isSet());
+  constexpr auto bitProxy2 = b.getBitProxy(2ULL);
+  EXPECT_TRUE(bitProxy2.isSet());
 }
  
  
--- include/Utils/BitField.h
+++ include/Utils/BitField.h
@@ -29,7 +29,7 @@ namespace cpptools {
  * @class BitField 
  * @brief BitField represents a fixed size set of bits.
  */
-template <std::size_t Size, std::size_t Index = 0Ull>
+template <std::size_t Size, std::size_t StartBit = 0Ull>
 class BitField {
 
 template <std::size_t Size>
@@ -43,10 +43,10 @@ public:
 
   // ---------------------------------------------------
   // public types
-  using value_type = MinimumType<Index + Size>;
+  using value_type = MinimumType<StartBit + Size>;
 
-  static constexpr value_type mask = (1U << Size) - 1U;
-  static constexpr value_type shiftedMask = mask << Index;
+  static constexpr value_type mask = (1U << Size) - 1U;        // BitField<3, 2> --> 0000 0111
+  static constexpr value_type cutter = mask << StartBit;  // BitField<3, 2> --> 0001 1100
 
   // ---------------------------------------------------
   // constructor
@@ -60,6 +60,15 @@ public:
   template <typename CharT, typename CharTraits = std::char_traits<CharT>>
   auto put (std::basic_ostream<CharT, CharTraits>& ostr) const -> std::basic_ostream<CharT, CharTraits>&;
 
+
+  template <bool IsConst>
+  class BitProxy;
+
+  constexpr auto getBitProxy(std::size_t index) const noexcept -> BitProxy<true>;
+  auto getBitProxy(std::size_t index) noexcept -> BitProxy<false>;
+
+
+
 private:
 
   // ---------------------------------------------------
@@ -72,27 +81,117 @@ private:
 };
 
 
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+class BitField<Size, StartBit>::BitProxy {
+
+  using value_type = typename BitField<Size, StartBit>::value_type;
+  static constexpr bool isConst = IsConst;
+
+  template <bool b>
+  struct refTypeT : meta::IdentityT<value_type&> {};
+  template <>
+  struct refTypeT<true> : meta::IdentityT<value_type const&> {};
+
+  template <bool b>
+  using refType = typename refTypeT<b>::type;
+
+  using reference = refType<isConst>;
+
+public:
+
+  // ---------------------------------------------------
+  // constructor
+  constexpr BitProxy(reference val, std::size_t index) noexcept;
+  BitProxy(BitProxy const&) = default;
+
+  // ---------------------------------------------------
+  // access operations
+  auto operator= (bool b) noexcept -> BitProxy&;
+  operator bool  () const noexcept;
+  void flip      () noexcept;
+  auto isSet     () const noexcept -> bool;
+
+private:
+
+  static constexpr value_type cutter = BitField<Size, StartBit>::cutter;
+
+  // ---------------------------------------------------
+  // private data
+  reference         value;
+  const value_type  mask;
+};
+
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+constexpr BitField<Size, StartBit>::BitProxy<IsConst>::BitProxy(reference val, std::size_t index) noexcept
+  : value {val}
+  , mask  {1U << (StartBit + index)}
+{}
+
+/**
+ * @brief Assigns a bool to the referenced bit.
+ */
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+inline auto BitField<Size, StartBit>::BitProxy<IsConst>::operator=(bool b) noexcept -> BitProxy&
+{
+  const value_type tmp = b ? (value & cutter) | mask : (value & cutter) & ~mask;
+  value = (value & ~cutter) | tmp;
+  return *this;
+}
+
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+inline auto BitField<Size, StartBit>::BitProxy<IsConst>::isSet() const noexcept -> bool
+{
+  return value & mask;
+}
+
+/**
+ * @brief Returns the value of the refenced bit.
+ */
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+inline BitField<Size, StartBit>::BitProxy<IsConst>::operator bool() const noexcept
+{
+  return value & mask;
+}
+
+/**
+ * @brief Flips the refenced bit.
+ */
+template <std::size_t Size, std::size_t StartBit>
+template <bool IsConst>
+inline void BitField<Size, StartBit>::BitProxy<IsConst>::flip() noexcept
+{
+  *this = *this ? false : true;
+}
+
+
+
+
 /**
  * @brief Constructor. Initializes the bits with the binary representation of the given value.
  */
-template <std::size_t Size, std::size_t Index>
-constexpr BitField<Size, Index>::BitField(value_type v)
+template <std::size_t Size, std::size_t StartBit>
+constexpr BitField<Size, StartBit>::BitField(value_type v)
   : value {getValue(v)}
 {}
 
 /**
  * @brief Copy Constructor.
  */
-template <std::size_t Size, std::size_t Index>
-constexpr BitField<Size, Index>::BitField(BitField<Size, Index> const& src)
+template <std::size_t Size, std::size_t StartBit>
+constexpr BitField<Size, StartBit>::BitField(BitField<Size, StartBit> const& src)
   : value {getValue(src.value)}
 {}
 
 /**
  * @brief Assignment operator.
  */
-template <std::size_t Size, std::size_t Index>
-inline auto BitField<Size, Index>::operator=(value_type v) noexcept -> BitField&
+template <std::size_t Size, std::size_t StartBit>
+inline auto BitField<Size, StartBit>::operator=(value_type v) noexcept -> BitField&
 {
   value = getValue(v);
   return *this;
@@ -101,45 +200,60 @@ inline auto BitField<Size, Index>::operator=(value_type v) noexcept -> BitField&
 /**
  * @brief Conversion operator.
  */
-template <std::size_t Size, std::size_t Index>
-constexpr BitField<Size, Index>::operator value_type() const noexcept
+template <std::size_t Size, std::size_t StartBit>
+constexpr BitField<Size, StartBit>::operator value_type() const noexcept
 { 
-  return (value >> Index) & mask;
+  return (value >> StartBit) & mask;
 }
 
 /**
  * @brief  Accesses the bit at a given position.
  * @return returns true if the bit is set, false otherwise.
  */
-template <std::size_t Size, std::size_t Index>
-constexpr auto BitField<Size, Index>::operator[](std::size_t position) const noexcept -> bool
+template <std::size_t Size, std::size_t StartBit>
+constexpr auto BitField<Size, StartBit>::operator[](std::size_t position) const noexcept -> bool
 {
-  return value & (1U << (Index + position));
+  return value & (1U << (StartBit + position));
 }
 
 
-template <std::size_t Size, std::size_t Index>
-constexpr auto BitField<Size, Index>::getValue(value_type v) noexcept -> value_type
+template <std::size_t Size, std::size_t StartBit>
+constexpr auto BitField<Size, StartBit>::getValue(value_type v) noexcept -> value_type
 {
-  return (v & ~shiftedMask) | ((v & mask) << Index);
+  return (v & ~cutter) | ((v & mask) << StartBit);
 }
+
+template <std::size_t Size, std::size_t StartBit>
+constexpr auto BitField<Size, StartBit>::getBitProxy(std::size_t index) const noexcept -> BitProxy<true>
+{
+  BitProxy<true> p(value, index);
+  return p;
+}
+
+template <std::size_t Size, std::size_t StartBit>
+inline auto BitField<Size, StartBit>::getBitProxy(std::size_t index) noexcept -> BitProxy<false>
+{
+  BitProxy<false> p(value, index);
+  return p;
+}
+
 
 /**
  * @brief Prints the binary representation of the BitField into a given stream.
  */
-template <std::size_t Size, std::size_t Index>
+template <std::size_t Size, std::size_t StartBit>
 template <typename CharT, typename CharTraits>
-inline auto BitField<Size, Index>::put(std::basic_ostream<CharT, CharTraits>& ostr) const -> std::basic_ostream<CharT, CharTraits>&
+inline auto BitField<Size, StartBit>::put(std::basic_ostream<CharT, CharTraits>& ostr) const -> std::basic_ostream<CharT, CharTraits>&
 {
-  cpptools::binaryRep(static_cast<value_type>(value & shiftedMask), ostr);
+  cpptools::binaryRep(static_cast<value_type>(value & cutter), ostr);
   return ostr;
 }
 
 /**
  * @brief Stream operator for bit fields.
  */
-template <std::size_t Size, std::size_t Index, typename CharT, typename CharTraits = std::char_traits<CharT>>
-inline auto operator<<(std::basic_ostream<CharT, CharTraits>& ostr, BitField<Size, Index> const& b) -> std::basic_ostream<CharT, CharTraits>&
+template <std::size_t Size, std::size_t StartBit, typename CharT, typename CharTraits = std::char_traits<CharT>>
+inline auto operator<<(std::basic_ostream<CharT, CharTraits>& ostr, BitField<Size, StartBit> const& b) -> std::basic_ostream<CharT, CharTraits>&
 {
   return b.put(ostr);
 }
